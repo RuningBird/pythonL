6677c76d457eb74b52fb9e7d13ee1cdcf39dcf87
ipower <ipowerink@outlook.com>
2017-05-24 16:10:40 +0800

    爬虫：连接网页，并解码

---
+++ day01/pachong.py
@@ -0,0 +1,15 @@
+#############################################爬 虫################################################
+###模块 urllib=url+lib
+# url 三部分：协议，主机名：端口，资源具体地址
+# protocol://hostname[:port]/path/[parameters][?query] #经常可以忽略第三部分
+
+
+
+import urllib.request
+
+httpResponse = urllib.request.urlopen('http://www.fishc.com')
+print(type(httpResponse))
+htlm = httpResponse.read()##这是二进制的，需要解码
+# print(htlm)
+html1 = htlm.decode('utf-8')
+print(html1)
